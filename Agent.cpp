0ef489849262deaa3881f631512698df49531542
Jonas <jgoltz@hm.edu>
2018-04-09 10:59:43 +0200

    Agent.qValues is not a pointer anymore

--- Agent.cpp
+++ Agent.cpp
@@ -26,7 +26,7 @@ Agent::Agent(double learningRate, double discountRate, double explRate) {
 
     pair<int, int> size = QValueSize;
     cout << "init qValues - size: " << size.first << "," << size.second << endl;
-    this->qValues = new Num2DTable(&size); //@todo get size of game for this
+    this->qValues = Num2DTable(&size); //@todo get size of game for this
     this->learningRate = learningRate;
     this->discountRate = discountRate;
     this->explRate = explRate;
@@ -49,39 +49,39 @@ void Agent::playGame() {
         counter++;
     }
     cout << counter<< " " << finished << endl;
-    cout << qValues->toString() << endl << endl;
+    cout << qValues.toString() << endl << endl;
 }
 
 void Agent::updateQValues(Environment::Response response) {
     //cout << "updateQValues" << endl;
-    double q = (*qValues)[*currentState];
+    double q = qValues[*currentState];
     q += learningRate * (response.reward + discountRate * (maxExpected(response.state))->first) - q;
-    (*qValues).setQValue(*currentState, q);
+    qValues.setQValue(*currentState, q);
 }
 
 //@todo implement maxExpected()
 pair<double, int>* Agent::maxExpected(pair<int, int> *state) {
 
     pair<int, int> *testAt = new pair<int, int>{state->first - 1, state->second};
-    double maxVal = (*qValues)[*testAt];
+    double maxVal = qValues[*testAt];
     int direction = 0;
 
     testAt = new pair<int, int>{state->first, state->second + 1};
-    if ((*qValues)[*testAt] > maxVal) {
-        maxVal = (*qValues)[*testAt];
+    if (qValues[*testAt] > maxVal) {
+        maxVal = qValues[*testAt];
         direction = 1;
     }
 
 
     testAt = new pair<int, int>{state->first + 1, state->second};
-    if ((*qValues)[*testAt] > maxVal) {
-        maxVal = (*qValues)[*testAt];
+    if (qValues[*testAt] > maxVal) {
+        maxVal = qValues[*testAt];
         direction = 2;
     }
 
     testAt = new pair<int, int>{state->first, state->second - 1};
-    if ((*qValues)[*testAt] > maxVal) {
-        maxVal = (*qValues)[*testAt];
+    if (qValues[*testAt] > maxVal) {
+        maxVal = qValues[*testAt];
         direction = 3;
     }
 
--- Agent.h
+++ Agent.h
@@ -15,13 +15,14 @@ using namespace std;
 
 class Agent {
 
-    Num2DTable * qValues;
+    Num2DTable qValues;
     Environment environment;
     pair<int, int> * currentState; //current state the agent is in
     double learningRate; //how fast he adopts to things he sees
     double discountRate; //how much value is given to future rewards
     int environmentType;//not needed for now but will be as soon as we have different environments
     //vector<int> actionCounter {};
+
 public:
     /*
      * constructor
