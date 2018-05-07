da800f1f6633bd435fa97c2b240bcecd0eeaac60
raharth <goltz.jonas@googlemail.com>
2018-05-07 09:34:01 +0200

    cleaning

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 //#include "Policy.h"
 #include "googletest/include/gtest/gtest.h"
 #include <cmath>
+//#include <armadillo/include/armadillo>
+
 
 using namespace std;
+//using namespace arma;
 
 int main(int argc, char* argv[]) {
+    //mat A = randu<mat>(4,5);
     Agent a = Agent(0.1, 0.8, 0.8, 0);
     a.fit(1000);
     cout << "main done" << endl;
